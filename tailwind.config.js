d728b15c01a55ea71db04c593be77b3c71c65c9e
Elvin <elvinsavio.lp@gmail.com>
2024-03-13 01:28:04 +0530

    add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,32 @@
 module.exports = {
   content: ["./templates/*/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: 'hsl(0, 0%, 3.9%)',
+        foreground: 'hsl(0, 0%, 98%)',
+        card: 'hsl(0, 0%, 3.9%)',
+        'card-foreground': 'hsl(0, 0%, 98%)',
+        popover: 'hsl(0, 0%, 3.9%)',
+        'popover-foreground': 'hsl(0, 0%, 98%)',
+        primary: 'hsl(0, 72.2%, 50.6%)',
+        'primary-foreground': 'hsl(0, 85.7%, 97.3%)',
+        secondary: 'hsl(0, 0%, 14.9%)',
+        'secondary-foreground': 'hsl(0, 0%, 98%)',
+        muted: 'hsl(0, 0%, 14.9%)',
+        'muted-foreground': 'hsl(0, 0%, 63.9%)',
+        accent: 'hsl(0, 0%, 14.9%)',
+        'accent-foreground': 'hsl(0, 0%, 98%)',
+        destructive: 'hsl(0, 62.8%, 30.6%)',
+        'destructive-foreground': 'hsl(0, 0%, 98%)',
+        border: 'hsl(0, 0%, 14.9%)',
+        input: 'hsl(0, 0%, 14.9%)',
+        ring: 'hsl(0, 72.2%, 50.6%)',
+      },
+      borderRadius: {
+        radius: '0.5rem', // Assuming you want to add a single radius value named 'radius'
+      },
+    },
   },
   plugins: [],
-}
-
+};
