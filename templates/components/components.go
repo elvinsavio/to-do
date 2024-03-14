b55d3b384f0c5557b6077548893da2a07b3351f5
Elvin <elvinsavio.lp@gmail.com>
2024-03-15 01:02:42 +0530

    add basic components

--- templates/components/components.go
+++ templates/components/components.go
@@ -4,7 +4,6 @@ import (
 	CardComponent "elvinsavio/todo/templates/components/card"
 	DividerComponent "elvinsavio/todo/templates/components/divider"
 	FormComponent "elvinsavio/todo/templates/components/form"
-	HeadingComponent "elvinsavio/todo/templates/components/heading"
 	InputComponent "elvinsavio/todo/templates/components/input"
 )
 
@@ -12,6 +11,5 @@ var (
 	Card    = CardComponent.Card
 	Divider = DividerComponent.Divider
 	Form    = FormComponent.Form
-	Heading = HeadingComponent.Heading
 	Input   = InputComponent.Input
 )
