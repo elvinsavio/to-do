088009f1edd3757956df848e1e63fcf134a8f81c
Elvin <elvinsavio.lp@gmail.com>
2024-03-14 09:53:31 +0530

    add exported components

---
+++ templates/components/components.go
@@ -0,0 +1,17 @@
+package components
+
+import (
+	CardComponent "elvinsavio/todo/templates/components/card"
+	DividerComponent "elvinsavio/todo/templates/components/divider"
+	FormComponent "elvinsavio/todo/templates/components/form"
+	HeadingComponent "elvinsavio/todo/templates/components/heading"
+	InputComponent "elvinsavio/todo/templates/components/input"
+)
+
+var (
+	Card    = CardComponent.Card
+	Divider = DividerComponent.Divider
+	Form    = FormComponent.Form
+	Heading = HeadingComponent.Heading
+	Input   = InputComponent.Input
+)
