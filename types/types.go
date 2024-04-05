61f4349d1c9fc942399cebffc7ddcc1ff3f5c7b5
Elvin <elvinsavio.lp@gmail.com>
2024-04-05 20:07:24 +0530

    add tag struct

--- types/types.go
+++ types/types.go
@@ -7,3 +7,14 @@ type ProjectList struct {
 	CreatedAt  time.Time
 	LastOpened time.Time
 }
+
+type Color struct {
+	R int `bson:"r"`
+	G int `bson:"g"`
+	B int `bson:"b"`
+}
+
+type Tag struct {
+	Name  string
+	Color Color
+}
