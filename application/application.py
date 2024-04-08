05f1502201359732590461b3484b1a257ad504b4
elvinsavio <elvinsavio.lp@gmail.com>
2024-04-09 02:24:19 +0530

    move loading env to const class

--- application/application.py
+++ application/application.py
@@ -4,8 +4,6 @@
     - logger
 """
 
-import tomllib
-import sys
 from application._logger import Logger
 from application._constants import Constants
 
@@ -14,21 +12,7 @@ def setup_application():
     """
     Sets up the application for flask
     """
-    _env = _load_toml_file()
-    constants = Constants(_env)
+    constants = Constants()
     logger = Logger(constants.LOGS)
-    logger.info("Logger initialized")
+    logger.info("Application initialized (constants, logger)")
     return (logger, constants)
-
-
-def _load_toml_file():
-    """
-    Loads local env file into memory
-    Application exits if not found
-    """
-    try:
-        with open("env.toml", "rb") as f:
-            data = tomllib.load(f)
-            return data
-    except FileNotFoundError:
-        sys.exit("Environment file not found")
