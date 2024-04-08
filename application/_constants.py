1b7f8a470503d6de1e961b9d65a514155917990f
elvinsavio <elvinsavio.lp@gmail.com>
2024-04-09 02:23:59 +0530

    add constant class for global constants

--- application/_constants.py
+++ application/_constants.py
@@ -1,5 +1,25 @@
-class Constants:
-    def __init__(self, env) -> None:
-        self.APPLICATION = env['application']
-        self.LOGS = env['logs']
+# pylint: disable=missing-module-docstring,too-few-public-methods
+import tomllib
+import sys
 
+class Constants:
+    """
+        Class containing application constants
+    """
+    def __init__(self) -> None:
+        self._env = self._load_toml_file()
+        self.APPLICATION = self._env["application"]
+        self.LOGS = self._env["logs"]
+
+    @staticmethod
+    def _load_toml_file():
+        """
+        Loads local env file into memory
+        Application exits if not found
+        """
+        try:
+            with open("env.toml", "rb") as f:
+                data = tomllib.load(f)
+                return data
+        except FileNotFoundError:
+            sys.exit("Environment file not found")
