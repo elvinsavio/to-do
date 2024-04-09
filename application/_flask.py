7940227329a2450fed1ddb30f5b585fc7681f5d2
elvinsavio <elvinsavio.lp@gmail.com>
2024-04-09 11:11:25 +0530

    add debug option

--- application/_flask.py
+++ application/_flask.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from application._logger import Logger
 
 
-def create_flask_app(settings):
+def create_flask_app(settings: dict[str, any], logger: Logger) -> Flask:
     """
-        Create an instance of flask
+    Create an instance of flask
     """
+    logger.info(f"Creating flask app - {settings['name']}")
     app = Flask(settings["name"])
+
+    if settings["debug"]:
+        logger.info("Debug mode - on")
+        app.debug = settings["debug"]
 
     return app
--- application/application.py
+++ application/application.py
@@ -4,17 +4,26 @@
     - logger
 """
 
+from time import perf_counter
+
+
 from application._logger import Logger
 from application._constants import Constants
 from application._flask import create_flask_app
+from routes import create_routes
 
 
 def setup_application():
     """
     Sets up the application for flask
     """
+    start_time = perf_counter()
+
     constants = Constants()
     logger = Logger(constants.LOGS)
-    logger.info("Application initialized (constants, logger)")
-    flask = create_flask_app(constants.APPLICATION)
+    flask = create_flask_app(constants.APPLICATION, logger)
+    flask = create_routes(flask)
+
+    end_time = perf_counter() - start_time
+    logger.info(f"Application started {end_time}s")
     return (logger, constants, flask)
