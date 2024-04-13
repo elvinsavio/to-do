568e948b1adeb8340b9f87383f097bac04bfadba
elvinsavio <elvinsavio.lp@gmail.com>
2024-04-14 00:09:09 +0530

    feat: remove logging

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,4 @@
 from application.application import setup_application
 
 
-logger, constants, flask, master_database = setup_application()
+logger, constants, flask = setup_application()
--- application/application.py
+++ application/application.py
@@ -3,15 +3,9 @@
     - constants
     - logger
 """
-
-from time import perf_counter
-import os
-
-
 from application._logger import Logger
 from application._constants import Constants
 from application._flask import create_flask_app
-from application._database import Master_Database
 from routes import create_routes
 
 
@@ -19,14 +13,9 @@ def setup_application():
     """
     Sets up the application for flask
     """
-    start_time = perf_counter()
-    os.system('cls' if os.name == 'nt' else 'clear')
     constants = Constants()
     logger = Logger(constants.LOGS)
-    master_db = Master_Database(constants.DATABASE, logger)
     flask = create_flask_app(constants.APPLICATION, logger)
     flask = create_routes(flask)
 
-    end_time = perf_counter() - start_time
-    logger.info(f"Application started {end_time}s")
-    return (logger, constants, flask, master_db)
+    return (logger, constants, flask)
