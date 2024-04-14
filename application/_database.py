a323bc81e6d5cedf62cdf64598088daba6239c67
elvinsavio <elvinsavio.lp@gmail.com>
2024-04-14 22:57:15 +0530

    refactor: db init

--- application/_database.py
+++ application/_database.py
@@ -1,52 +1,44 @@
 import sqlite3
-import datetime
+from sqlite3 import Connection
 import sys
-from pathlib import Path
+from flask import g
 
-# class Master_Database:
-#     def __init__(self, settings: dict[str, str], logger):
-#         self.path = settings["path"]
-#         self.name = settings["name"]
-#         self.logger = logger
-#         self.conn = None
-#         self.cursor = None
-#         self._create_path_if_not_exists(self.path)
 
-#         # No database operations should be done in __init__
+def create_database(settings: dict[str, str]):
+    """
+    Wrapper class to support constants
+    args:
+        setting: env var
 
-#     def _create_path_if_not_exists(self, path):
-#         """
-#         Creates the log output folder
-#         """
-#         Path(path).mkdir(parents=True, exist_ok=True)
+    return:
+        func
+    """
 
-#     async def connect_to_master_db(self):
-#         try:
-#             self.conn = await sqlite3.connect(f"{self.path}/{self.name}.db")
-#             self.cursor = self.conn.cursor()
-#             self.logger.info(f"Connected to database - {self.name}")
-#         except sqlite3.Error as e:
-#             self.logger.error(e)
-#             sys.exit(f"Failed to connect to database! - {self.name}")
+    def database(db_type: str, db_name: str) -> Connection:
+        """
+        Create a new database connection and
+        store in the flask context
 
-#     async def create_project(self, name: str, description: str):
-#         """
-#         Inserts a new row into the master db
-#         args:
-#             name: str - name of project
-#             description: str - some description of the project
-#         return:
-#             ok, err
-#         """
-#         try:
-#             await self.connect_to_master_db()  # Await the connection
-#             created_at = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#             last_modified = created_at
-#             self.cursor.execute("INSERT INTO master (title, description, created_at, last_modified) VALUES (?, ?, ?, ?)",
-#                                 (name, description, created_at, last_modified))
-#             self.conn.commit()
-#             self.conn.close()
-#             return "ok"
-#         except sqlite3.Error as e:
-#             self.logger.error(e)
-#             return "err"
+        args:
+            db_type: master/<project_name> for accessing the database
+            dn_name: db_type == <project_name> for accessing the database
+
+        returns:
+            db connection
+        """
+        if db_type == "master":
+            db = getattr(g, "_database", None)
+            if db is None:
+
+                path = settings.get("path", False)
+                name = settings.get("name", False)
+                db_url = f"{path}/{name}.db"
+                if not path or not name:
+                    sys.exit("Environment file not found")
+                db = g._database = sqlite3.connect(db_url)
+
+            return db
+        elif db_type == "project":
+            ...
+
+    return database
