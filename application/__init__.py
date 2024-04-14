927224bdb88ffb668794dab6f413f128947d42cd
elvinsavio <elvinsavio.lp@gmail.com>
2024-04-14 22:57:01 +0530

    move app init to __init__

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,15 @@
-from application.application import setup_application
+"""
+Sets up the application for flask
+"""
 
+from application._logger import Logger
+from application._constants import Constants
+from application._flask import create_flask_app
+from application._cli import register_cli_commands
+from application._database import create_database
 
-logger, constants, flask = setup_application()
+constants = Constants()
+logger = Logger(constants.LOGS)
+database = create_database(constants.DATABASE)
+flask = create_flask_app(constants.APPLICATION)
+register_cli_commands(flask, constants.DATABASE)
