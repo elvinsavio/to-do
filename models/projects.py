724dd8c6009c50caeb200684d643a63814504778
elvinsavio <elvinsavio.lp@gmail.com>
2024-04-15 23:29:53 +0530

    add route to create projects

--- models/projects.py
+++ models/projects.py
@@ -11,7 +11,8 @@ def create_new_project(name: str, description: str = None):
         description: <optional> description for project
     """
     try:
-        db = database("master", None)
+        # create an entry in the master db
+        db = database("master")
         cursor = db.cursor()
         time_now = datetime.now()
         cursor.execute(
@@ -22,13 +23,59 @@ def create_new_project(name: str, description: str = None):
             """,
             (name, description, time_now, time_now),
         )
+
+        # create the projects tables
+        project_db = database(name)
+        project_cursor = project_db.cursor()
+
+        # task table
+        project_cursor.execute(
+            """
+            CREATE TABLE task (
+                id INT PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                type_id INT,
+                priority_id INT,
+                due_date TEXT,
+                status TEXT,
+                FOREIGN KEY (type_id) REFERENCES task_type(id),
+                FOREIGN KEY (priority_id) REFERENCES task_priority(id)
+            );
+            """
+        )
+
+        project_cursor.execute(
+            """
+            CREATE TABLE priority (
+                id INT PRIMARY KEY,
+                name TEXT NOT NULL,
+                color TEXT
+            );
+            """
+        )
+
+        project_cursor.execute(
+            """
+            CREATE TABLE type (
+                id INT PRIMARY KEY,
+                name TEXT NOT NULL,
+                color TEXT
+            );
+            """
+        )
+
+        project_db.commit()
         db.commit()
+
         return "ok"
+    
     except IntegrityError as e:
         logger.error("Duplicate warning error :", e)
         return f"{name} already exists."
+
     except Exception as e:
         logger.error("Failed to insert into database :", e)
         return "Something went wrong!"
     finally:
+        project_cursor.close()
         db.close()
--- routes/projects.py
+++ routes/projects.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request, redirect
+
 import models.projects as p
+from application import constants
 
 _projects = Blueprint(
     "projects_bp",
@@ -11,11 +13,32 @@ _projects = Blueprint(
 
 @_projects.route("/new", methods=["GET"])
 def new_project():
+    """
+    Handler for create project page
+
+    request:
+        None
+
+    response:
+        Renders the page
+    """
     return render_template("new.html")
 
 
 @_projects.route("/new", methods=["POST"])
 def create_project():
+    """
+    Handler for create projects form
+
+    request:
+        project_name: name of project
+        description: <optional> description for project
+
+    response:
+        renders the page new project page
+        renders the page new project page with errors
+        redirects to the new created project
+    """
     form_data = request.form
     project_name = form_data.get("name", False)
     description = form_data.get("description", False)
@@ -23,8 +46,12 @@ def create_project():
     if not project_name:
         return render_template("new.html", error="Name is required")
 
+    # checks if name is same as master db name
+    master_db_name = constants.DATABASE.get("name", None)
+    if project_name == master_db_name:
+        return render_template("new.html", error=f"Name cannot be {master_db_name}")
+
     res = p.create_new_project(name=project_name, description=description)
-    print(res)
     if res == "ok":
         return redirect("/")
 
